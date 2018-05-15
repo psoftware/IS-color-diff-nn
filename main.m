0ac19657664df54ce51089a0dffc0494a154191e
Antonio Le Caldare <antonio.lecaldare@gmail.com>
2018-05-15 21:15:09 +0200

    usare rand invece che randn per la generazione del rumore. il rumore è ora generato tra -1/10 e 1/10

--- main.m
+++ main.m
@@ -9,9 +9,9 @@ specnoised = zeros(421,1269*copies); % for performance
 for i_spec = 1:1269
     for sampl = 1:copies
         specmaster(:, i_spec*sampl) = spectra(:, i_spec);
-        specnoised(1:140, i_spec*sampl) = spectra(1:140, i_spec) + randn(1,1)/75;
-        specnoised(141:280, i_spec*sampl) = spectra(141:280, i_spec) + randn(1,1)/75;
-        specnoised(281:421, i_spec*sampl) = spectra(281:421, i_spec) + randn(1,1)/75;
+        specnoised(1:140, i_spec*sampl) = spectra(1:140, i_spec) + (rand(1,1)*2 - 1)/10;
+        specnoised(141:280, i_spec*sampl) = spectra(141:280, i_spec) + (rand(1,1)*2 - 1)/10;
+        specnoised(281:421, i_spec*sampl) = spectra(281:421, i_spec) + (rand(1,1)*2 - 1)/10;
     end
 end
 

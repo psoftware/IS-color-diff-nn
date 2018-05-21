264ef64819a00cd3c20176b408aaddcedcf7140e
Antonio Le Caldare <antonio.lecaldare@gmail.com>
2018-05-22 00:07:35 +0200

    main: computed new best features, getting R 0.96 with error 0.135

--- main.m
+++ main.m
@@ -30,14 +30,21 @@ distance = de(labnoise', labmaster')'
 features = compute_features(specmaster, specnoised, copies);
 
 opt = statset('display', 'iter');
-%[fs, history] = sequentialfs(@fs_net, features, distance', 'cv', 'none', 'opt', opt, 'nfeatures', 1);
+%[fs, history] = sequentialfs(@fs_net, features, distance', 'cv', 'none', 'opt', opt, 'nfeatures', 12);
 
 % compute best network
 %bestfeatures = features(:,fs);
 
 %selected best 10 features - parabola regression
 %bestfeatures = features(:, [true false false false true false false true false false true false false false false true false false false false false false true false false false false false false false true false true false false false false false true true false false false false false false false false]);
-bestfeatures = features(:, [true true false false false false false true false false false false false false true true false false false false false false true false true false false false false false true false true false false true false false false false false false]);
+%bestfeatures = features(:, [true true false false false false false true false false false false false false true true false false false false false false true false true false false false false false true false true false false true false false false false false false]);
+
+% 10 features, 10 neurons (0.84)
+%bestfeatures = features(:, boolean([0   0   0   0   0   0   1   1   0   0   0   0   0   1   0   0   1   0   0   0   0   0   0   0   0   0   0   1   1   0   0  0  0   0   1   0   0   1   0   1   0   1]));
+% 12 features, 10 neurons (0.93) (others,skewness,sum,NO regression)
+%bestfeatures = features(:, boolean([1   0   0   0   0   0   1   1   1   0   1   0   0   0   0   1   0   0   0   0   0   0   0   0   0   0   0   1   1   0   0   1   0   0   1   0   1   0   0   1   0   0]));
+% 12 features, 10 neurons (0.963, 0.135) (others,skewness,sum,regression)
+bestfeatures = features(:, boolean([0   1   0   0   0   0   1   1   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   1   1   0   0   0   1   0   0   0   0   0   0   1   0   0   1   0   1   0   1   0   0   1   0   0   1   0]));
 
 
 mse = fs_net_final(bestfeatures, distance');

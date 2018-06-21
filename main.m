5149b68bc8f4024a8c2c94b119e2d750be2940d7
enzoping95 <vincenzo.consales95@gmail.com>
2018-06-22 00:53:43 +0200

    best 10 features for adjusted_de

--- main.m
+++ main.m
@@ -1,6 +1,6 @@
 clear();
 
-load("dataset\IS_dataset.mat")
+load('dataset\IS_dataset.mat')
 
 copies = 10;
 seed = 27;
@@ -12,7 +12,8 @@ seed = 27;
 [labmaster, labnoise] = compute_lab(specmaster, specnoised, copies);
 
 % compute DeltaE
-distance = de(labnoise', labmaster')'
+distance = de(labnoise', labmaster')';
+distance_adj = compute_corrected_deltaE(labmaster', distance);
 
 % figure('Position', [100, 100, 1000, 400]);
 % subplot(1,2,1)
@@ -30,22 +31,23 @@ distance = de(labnoise', labmaster')'
 features = compute_features(specmaster, specnoised, copies);
 
 opt = statset('display', 'iter');
-%[fs, history] = sequentialfs(@fs_net, features, distance', 'cv', 'none', 'opt', opt, 'nfeatures', 12);
+%[fs, history] = sequentialfs(@fs_net, features, distance', 'cv', 'none', 'opt', opt, 'nfeatures', 5);
+%[fs, history] = sequentialfs(@fs_net, features, distance_adj, 'cv', 'none', 'opt', opt, 'nfeatures', 10);
 
 % compute best network
 %bestfeatures = features(:,fs);
-
-%selected best 10 features - parabola regression
-%bestfeatures = features(:, [true false false false true false false true false false true false false false false true false false false false false false true false false false false false false false true false true false false false false false true true false false false false false false false false]);
-%bestfeatures = features(:, [true true false false false false false true false false false false false false true true false false false false false false true false true false false false false false true false true false false true false false false false false false]);
 
 % 10 features, 10 neurons (0.84)
 %bestfeatures = features(:, boolean([0   0   0   0   0   0   1   1   0   0   0   0   0   1   0   0   1   0   0   0   0   0   0   0   0   0   0   1   1   0   0  0  0   0   1   0   0   1   0   1   0   1]));
 % 12 features, 10 neurons (0.93) (others,skewness,sum,NO regression)
 %bestfeatures = features(:, boolean([1   0   0   0   0   0   1   1   1   0   1   0   0   0   0   1   0   0   0   0   0   0   0   0   0   0   0   1   1   0   0   1   0   0   1   0   1   0   0   1   0   0]));
 % 12 features, 10 neurons (0.963, 0.135) (others,skewness,sum,regression)
-bestfeatures = features(:, boolean([0   1   0   0   0   0   1   1   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   1   1   0   0   0   1   0   0   0   0   0   0   1   0   0   1   0   1   0   1   0   0   1   0   0   1   0]));
+%bestfeatures = features(:, boolean([0   1   0   0   0   0   1   1   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   0   1   1   0   0   0   1   0   0   0   0   0   0   1   0   0   1   0   1   0   1   0   0   1   0   0   1   0]));
 
+%10 best - 10 neurons - deAdju
+bestfeatures = features(:,[true true false false false false false false false false false false false false false false false false false false false false false false true false false true false false true true false false false false false false false false false false false false false false false false false false false false false false true false false false false false false false false false false false false true true true false false])
+%bestfeatures = features(:, [false false false false false false false false false false false false false false false false false false false false false false false false false true false false false false false false false false false false false false false false false false false false false false false false false false false false false false false true false true false false false false false false false true true false false false false false]);
 
-mse = fs_net_final(bestfeatures, distance');
+%mse = fs_net_final(bestfeatures, distance');
+mse = fs_net_final(bestfeatures, distance_adj);
 disp(['Mean Squared Error = ', num2str(mse)]);

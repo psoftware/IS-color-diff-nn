59d995f71939eae74d8201908afd0fb01b08f045
Antonio Le Caldare <antonio.lecaldare@gmail.com>
2018-06-23 18:45:22 +0200

    fs_net_final: aggiunte repetition e scelta della rete migliore

--- fs_net_final.m
+++ fs_net_final.m
@@ -1,9 +1,35 @@
 function perf = fs_net_final(x,t)
-	hiddenLayerSize = 15
-    net = fitnet(hiddenLayerSize);
+	hiddenLayerSize = 15;
+    repetitions = 10;
+
+    best_mse = 999;
+    best_net = [];
+
+    disp(['fs_net_final started (', num2str(repetitions), ' repetitions)']);
+
     xx = x.';
-    tt = t.';
-    [net, tr] = train(net, xx, tt);
-    y = net(xx);
-    perf = perform(net, tt, y);
+	tt = t.';
+    for i=1:repetitions
+        net = fitnet(hiddenLayerSize);
+        net.trainParam.showWindow = false;
+
+        [net, tr] = train(net, xx, tt);
+        y = net(xx);
+        perf = perform(net, tt, y);
+
+        regr = regression(tt,y);
+        disp(['Repetition ', num2str(i), ' mse=', num2str(perf), ' R=', num2str(regr)]);
+        if(perf < best_mse)
+            best_mse = perf;
+            best_net = net;
+        end
+    end
+
+    % plot best network regression
+    [m,n] = size(x);
+    y = best_net(xx);
+    best_R = regression(tt,y);
+
+    disp(['Best neural network (', num2str(n),' features, ', num2str(hiddenLayerSize), ' neurons): mse=', num2str(best_mse), ' R=', num2str(best_R)]);
+    plotregression(tt,y, 'All');
 end

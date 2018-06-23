64c7f30f463a47c6f479f402c4a666b049777ab1
enzoping95 <vincenzo.consales95@gmail.com>
2018-06-23 12:30:10 +0200

    in the last sample we add more noise. This increase the number of copies clearDiff or totalDiff wrt master

--- gen_copies.m
+++ gen_copies.m
@@ -11,11 +11,16 @@ function [specmasterP, specnoisedP] = gen_copies(spectra, copies, seed)
                 %DIVIDE BY 1
                 %specnoisedP(1:421, (i_spec-1)*copies + sampl) = spectra(1:421, i_spec) + (rand(1,1)*2 - 1)/75;
                 specnoisedP(:, (i_spec-1)*copies + sampl) = spectra(:,i_spec) * random('unif',0.95,1.12); 
-            else
-            %DIVIDE BY 3
+            elseif ( sampl < copies )
+                %DIVIDE BY 3
                 specnoisedP(1:146, (i_spec-1)*copies + sampl) = spectra(1:146, i_spec) * random('unif',0.95,1.12);
                 specnoisedP(147:300, (i_spec-1)*copies + sampl) = spectra(147:300, i_spec) * random('unif',0.95,1.12);
                 specnoisedP(301:421, (i_spec-1)*copies + sampl) = spectra(301:421, i_spec) * random('unif',0.95,1.12);  
+            else 
+                %in the last sample the noise is bigger
+                specnoisedP(1:146, (i_spec-1)*copies + sampl) = spectra(1:146, i_spec) * random('unif',1,1.15);
+                specnoisedP(147:300, (i_spec-1)*copies + sampl) = spectra(147:300, i_spec) * random('unif',1,1.15);
+                specnoisedP(301:421, (i_spec-1)*copies + sampl) = spectra(301:421, i_spec) * random('unif',1,1.15);  
             end 
         end
     end
